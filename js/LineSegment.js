74fb181a250b3c4c5d5b107b3ce15d5d016ebbad
Brett Tolbert <bretttolbert@gmail.com>
2012-02-11 08:26:08 -0600

    completed LineSegment refactoring

--- js/LineSegment.js
+++ js/LineSegment.js
@@ -1,10 +1,28 @@
 ﻿function LineSegment(Ax,Ay,Bx,By) 
 {
-    this.p1 = {x:Ax, y:Ay};
-    this.p2 = {x:Bx, y:By};
+    this.p1 = {x:0, y:0};
+    this.p2 = {x:0, y:0};
+    if (Ax !== undefined) this.p1.x = Ax;
+    if (Ay !== undefined) this.p1.y = Ay;
+    if (Bx !== undefined) this.p2.x = Bx;
+    if (By !== undefined) this.p2.y = By;
     this.selected = false;
     this.hover = false;
+    
     this.repr = function() {
         return '{p1:{x:' + this.p1.x + ',y:' + this.p1.y + '},p2:{x:' + this.p2.x + ',y:' + this.p2.y + '}';
+    }
+    
+    this.setData = function(Ax,Ay,Bx,By) {
+        this.p1.x = Ax;
+        this.p1.y = Ay;
+        this.p2.x = Bx;
+        this.p2.y = By;
+    }
+    
+    this.clone = function() {
+        var result = new LineSegment();
+        result.setData(this.p1.x, this.p1.y, this.p2.x, this.p2.y);
+        return result;
     }
 }

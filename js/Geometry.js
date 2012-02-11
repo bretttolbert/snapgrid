d1f16bbfaa9a3e3a394b3722cfbe7354a74f1b85
Brett Tolbert <bretttolbert@gmail.com>
2012-02-10 21:16:28 -0600

    implemented join line segments tool

--- js/Geometry.js
+++ js/Geometry.js
@@ -1,4 +1,4 @@
-var Geometry = Geometry || {};
+﻿var Geometry = Geometry || {};
 
 /**
  * Determines the point C on the line segment defined by points A and B which is
@@ -40,4 +40,61 @@ Geometry.getClosestPointOnLineSegment = function(A, B, P, tolerance) {
 
 Geometry.distance = function(A, B) {
     return Math.sqrt(Math.pow(B.x - A.x, 2) + Math.pow(B.y - A.y, 2));
+}
+
+/**
+ * Calculates area of the triangle defined by the points ABC
+ */
+Geometry.areaOfTriangle = function(A,B,C) {
+    return (A.x * (B.y - C.y) + B.x * (C.y - A.y) + C.x * (A.y - B.y)) / 2
+}
+
+/**
+ * Returns the left, right, top or bottom-most point in a list of points
+ * Direction parameter specifies 'left', 'right', 'top' or 'bottom'
+ */
+Geometry.extrema = function(points, direction) {
+    if (points.length > 0) {
+        var most = points[0];
+        if (points.length == 1) {
+            return most;
+        } else {
+            for (var i=1; i<points.length; ++i) {
+                var condition = false;
+                if (direction == 'left') {
+                    condition = points[i].x < most.x;
+                } else if (direction == 'right') {
+                    condition = points[i].x > most.x;
+                } else if (direction == 'top') {
+                    condition = points[i].y < most.y;
+                } else if (direction == 'bottom') {
+                    condition = points[i].y > most.y;
+                }
+                if (condition) {
+                    most = points[i];
+                }
+            }
+            return most;
+        }
+    }
+}
+
+/** 
+ * Returns true if points are on the same line
+ */
+Geometry.onSameLine = function(points) {
+    if (points.length > 0) {
+        if (points.length < 3) {
+            return true;
+        } else {
+            var result = true;
+            for (var i=2; i<points.length; ++i) {
+                if (Geometry.areaOfTriangle(points[i],points[i-1],points[i-2]) != 0) {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
 }
---
+++ js/LineSegment.js
@@ -0,0 +1,10 @@
+﻿function LineSegment(Ax,Ay,Bx,By) 
+{
+    this.p1 = {x:Ax, y:Ay};
+    this.p2 = {x:Bx, y:By};
+    this.selected = false;
+    this.hover = false;
+    this.repr = function() {
+        return '{p1:{x:' + this.p1.x + ',y:' + this.p1.y + '},p2:{x:' + this.p2.x + ',y:' + this.p2.y + '}';
+    }
+}
